f47a5314b0061bf2b80ad486af41ebde0328e953
raju <araju2014to2018@gmail.com>
2020-01-08 11:28:16 +0530

    Initial Commit

---
+++ tableviewwithserverdata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableviewwithserverdata
+//
+//  Created by A RAJU on 1/8/20.
+//  Copyright © 2020 A RAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
